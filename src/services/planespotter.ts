d1caa6a625e3c053719a9970488f63bb90e7b52e
Lucas. <iaminvisy@gmail.com>
2025-10-21 18:28:59 +0100

    Update planespotter.ts
    apparently using a plane's `hex` wasnt specific enough, so now ive added `icaoType` to the url

--- src/services/planespotter.ts
+++ src/services/planespotter.ts
@@ -3,7 +3,11 @@ import type { PlaneSpotter, PlanePhoto, FlightInfo } from '../types'
 
 export async function getPlanespotterInfo(flight:FlightInfo): Promise<PlanePhoto | null> {
   try {
-    const res = await fetch(`https://api.planespotters.net/pub/photos/hex/${flight.hex}` + (flight.r ? `?reg=${flight.r}` : ''))
+    const url = new URL(`https://api.planespotters.net/pub/photos/hex/${flight.hex}`)
+    if (flight.r) url.searchParams.set('reg', flight.r)
+    if (flight.t) url.searchParams.set('icaoType', flight.t)
+
+    const res = await fetch(url)
     const d:PlaneSpotter = await res.json()
     if (!res.ok) return null
 
